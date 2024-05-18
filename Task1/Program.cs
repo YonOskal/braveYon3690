a63784b8a0a98cdff16a66cdb6dbe53f8a4cdc71
Yon <larin.andreika@yandex.ru>
2024-05-18 18:50:24 +0300

    add Task1 sem4

---
+++ Task1/Program.cs
@@ -0,0 +1,62 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+    int[] array = new int[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max);
+    }
+
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]}");
+        }
+    }
+    Console.Write("]");
+}
+
+int CountPrimeNumbers(int[] array)
+{
+    int count = 0;
+    // реализация
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (IsPrime(array[i]))
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+bool IsPrime(int num)
+{
+    for (int i = 2; i <= Math.Sqrt(num); i++)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+int[] arr = CreateArrayRndInt(10, 1, 100);
+PrintArray(arr);
+
+int countPrime = CountPrimeNumbers(arr);
+Console.Write($" => {countPrime}");
