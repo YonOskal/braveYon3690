d80626963fd969194335f6a4ed8aac8a6769b677
Yon <larin.andreika@yandex.ru>
2024-05-21 15:46:09 +0300

    add Task1 Sem4

---
+++ Task3/Program.cs
@@ -0,0 +1,55 @@
+﻿int[] CreateArrayRndInt(int size, int min, int max)
+{
+int[] array = new int[size];
+Random rnd = new Random();
+
+for (int i = 0; i < size; i++)
+{
+array[i] = rnd.Next(min, max);
+}
+
+return array;
+}
+
+void PrintArray(int[] array)
+{
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+if (i < array.Length - 1)
+{
+Console.Write($"{array[i]}, ");
+}
+else
+{
+Console.Write($"{array[i]}");
+}
+}
+Console.Write("]");
+}
+
+int ArrayToNumber(int[] array)
+{
+int res = 0;
+for (int i = 0; i < array.Length; i++)
+{
+res = res * 10 + array[i];
+}
+
+return res;
+}
+
+Console.WriteLine("Задайте размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if(n < 1 || n > 8 )
+{
+Console.WriteLine("Некорректный ввод!");
+return;
+}
+
+int[] arr = CreateArrayRndInt(n, 0, 10);
+PrintArray(arr);
+
+int result = ArrayToNumber(arr);
+Console.WriteLine($" => {result}");
---
+++ Task4/Program.cs
@@ -0,0 +1,67 @@
+﻿// Напишите программу, которая бесконечно запрашивает целые числа с консоли. Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+
+// Console.Write("Введите целое число: ");
+// string input = Console.ReadLine;
+// while (true)
+// {
+//     if (num == "q")
+//             {
+//                 Console.WriteLine("Программа завершена.");
+//                 break;
+//             }
+//     Console.Write("Введите целое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// if (n > 0)
+//         {
+//             Console.WriteLine("Некоректный ввод");
+//             return;
+//         }
+// }
+
+
+
+
+while (true)
+{
+    Console.Write("Введите целое число: ");
+    string input = Console.ReadLine();
+
+    if (input == "q")
+    {
+        Console.WriteLine("Программа завершена.");
+        break;
+    }
+
+    if (int.TryParse(input, out int n))
+    {
+        if (n < 0)
+        {
+            Console.WriteLine("Некорректный ввод");
+            continue;
+        }
+
+        if (IsSumOfDigitsEven(n))
+        {
+            Console.WriteLine("Сумма цифр числа чётная. Программа завершена.");
+            break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Некорректный ввод");
+    }
+}
+
+
+static bool IsSumOfDigitsEven(int number)
+{
+    int sum = 0;
+    while (number > 0)
+    {
+        sum += number % 10;
+        number /= 10;
+    }
+    return sum % 2 == 0;
+}
+
+
